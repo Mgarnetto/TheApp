0e63184562a5c7533fb4164664781b42b8693887
Marcus Garnetto <mgarnetto@hotmail.com>
2023-11-17 16:06:32 -0500

    Updated File class

--- TheApp/Objects/File.cs
+++ TheApp/Objects/File.cs
@@ -8,14 +8,15 @@
         public string fileTitle { get; set; } //varchar 100 more formal for display
         public string fileServer { get; set; } //varchar 20 server where file is hosted
         public string filePath { get; set; } //varchar 50 location on the server
-        public string fileType { get; set; } //varchar 10 audio/video/image
+        public string fileType { get; set; } //varchar 20 audio/video/image also profile / profile-default
+        public DateTime modifiedDateTime { get; set; } // when it was created/modified
 
         public File()
         {
             // Default constructor
         }
 
-        public File(int userID, string fileName, string fileTitle, string fileServer, string filePath, string fileType)
+        public File(int userID, string fileName, string fileTitle, string fileServer, string filePath, string fileType, DateTime modifiedDateTime)
         {
             this.userID = userID;
             this.fileName = fileName;
@@ -23,6 +24,7 @@
             this.fileServer = fileServer;
             this.filePath = filePath;
             this.fileType = fileType;
+            this.modifiedDateTime = modifiedDateTime;   
         }
     }
 
