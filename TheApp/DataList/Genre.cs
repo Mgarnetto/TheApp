b6bc77fd58c2651cabaf742e39bcf447a17b4d05
Marcus Garnetto <mgarnetto@hotmail.com>
2023-11-19 16:07:04 -0500

    State, Genre classes added, Map Click Event added

---
+++ TheApp/DataList/Genre.cs
@@ -0,0 +1,37 @@
+﻿namespace TheApp.DataList
+{
+    public class Genre
+    {
+        static String[] genres = new String[]
+        {
+            "Alternative",
+            "Anamoly",
+            "BluesJazz",
+            "Country",
+            "Dance",
+            "Disco",
+            "GoGo",
+            "HeavyMetal",
+            "HipHop",
+            "Instrumental",
+            "MixTape",
+            "Pop",
+            "Punk",
+            "R&B",
+            "Reggae",
+            "Rock&Roll",
+            "Soul",
+            "Spiritual",
+            "SpokenWord",
+            "Techno",
+            "Trap",
+            "WorldMusic"
+        };
+
+        public static String[] getGenres()
+        {
+            return genres;
+        }
+
+    }
+}
---
+++ TheApp/DataList/State.cs
@@ -0,0 +1,124 @@
+﻿namespace TheApp.DataList
+{
+    public class State
+    {
+        static String[] states = new String[]
+        {
+            "Alabama",
+            "Alaska",
+            "Arizona",
+            "Arkansas",
+            "California",
+            "Colorado",
+            "Connecticut",
+            "Delaware",
+            "Florida",
+            "Georgia",
+            "Hawaii",
+            "Idaho",
+            "Illinois",
+            "Indiana",
+            "Iowa",
+            "Kansas",
+            "Kentucky",
+            "Louisiana",
+            "Maine",
+            "Maryland",
+            "Massachusetts",
+            "Michigan",
+            "Minnesota",
+            "Mississippi",
+            "Missouri",
+            "Montana",
+            "Nebraska",
+            "Nevada",
+            "New Hampshire",
+            "New Jersey",
+            "New Mexico",
+            "New York",
+            "North Carolina",
+            "North Dakota",
+            "Ohio",
+            "Oklahoma",
+            "Oregon",
+            "Pennsylvania",
+            "Rhode Island",
+            "South Carolina",
+            "South Dakota",
+            "Tennessee",
+            "Texas",
+            "Utah",
+            "Vermont",
+            "Virginia",
+            "Washington",
+            "West Virginia",
+            "Wisconsin",
+            "Wyoming"
+        };
+
+        static String[] abbr = new String[]
+        {
+            "AL",
+            "AK",
+            "AZ",
+            "AR",
+            "CA",
+            "CO",
+            "CT",
+            "DE",
+            "FL",
+            "GA",
+            "HI",
+            "ID",
+            "IL",
+            "IN",
+            "IA",
+            "KS",
+            "KY",
+            "LA",
+            "ME",
+            "MD",
+            "MA",
+            "MI",
+            "MN",
+            "MS",
+            "MO",
+            "MT",
+            "NE",
+            "NV",
+            "NH",
+            "NJ",
+            "NM",
+            "NY",
+            "NC",
+            "ND",
+            "OH",
+            "OK",
+            "OR",
+            "PA",
+            "RI",
+            "SC",
+            "SD",
+            "TN",
+            "TX",
+            "UT",
+            "VT",
+            "VA",
+            "WA",
+            "WV",
+            "WI",
+            "WY"
+        };
+
+        public static String getState(String name)
+        {
+            for (int a = 0; a < states.Length; a++)
+            {
+                if (name.Equals(states[a]))
+                    return abbr[a];
+            }
+
+            return "null";
+        }
+    }
+}
